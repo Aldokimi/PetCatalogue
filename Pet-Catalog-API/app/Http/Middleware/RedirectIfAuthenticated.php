77014e255d111d255cc76622759f3252dd5d3e04
Kali <mnd7729@gmail.com>
2022-05-22 12:47:32 -0400

    Added authentication to the REAS-API of the application

--- Pet-Catalog-API/app/Http/Middleware/RedirectIfAuthenticated.php
+++ Pet-Catalog-API/app/Http/Middleware/RedirectIfAuthenticated.php
@@ -23,7 +23,10 @@ class RedirectIfAuthenticated
 
         foreach ($guards as $guard) {
             if (Auth::guard($guard)->check()) {
-                return redirect(RouteServiceProvider::HOME);
+                if($request->expectsJson()){
+                    return response()->json(["message" => 'Authenticated'], 200);
+                }
+                return response()->json(["message" => 'Unauthenticated'], 202);
             }
         }
 
--- Pet-Catalog-API/routes/api.php
+++ Pet-Catalog-API/routes/api.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::resource('/pets', PetController::class)->except(['create','edit']);
+Route::middleware('auth:sanctum')->resource('/pets', PetController::class)->except(['create','edit']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
