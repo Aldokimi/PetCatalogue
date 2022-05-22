c40a7c09c9ff6d884455530f1751183fa84c12ca
Kali <mnd7729@gmail.com>
2022-05-22 19:28:39 -0400

    Created base compnent for the application

--- Pet-Catalogue-Client/src/app/app-routing.module.ts
+++ Pet-Catalogue-Client/src/app/app-routing.module.ts
@@ -1,7 +1,38 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainPageComponent } from './main-page/main-page.component';
+import { PetDetailsComponent } from './pet-details/pet-details.component';
+import { PetEditComponent } from './pet-edit/pet-edit.component';
+import { PetFormComponent } from './pet-form/pet-form.component';
+import { PetListComponent } from './pet-list/pet-list.component';
+import { ProfileComponent } from './profile/profile.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    component : MainPageComponent
+  },
+  {
+    path: "pets",
+    component : PetListComponent
+  },
+  {
+    path: "pets/:id",
+    component : PetDetailsComponent
+  },
+  {
+    path: "pets/new",
+    component: PetFormComponent
+  },
+  {
+    path: "pets/:id/edit",
+    component: PetEditComponent
+  },
+  {
+    path : "profile",
+    component : ProfileComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Pet-Catalogue-Client/src/app/app.module.ts
+++ Pet-Catalogue-Client/src/app/app.module.ts
@@ -3,14 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { MainPageComponent } from './main-page/main-page.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { PetListComponent } from './pet-list/pet-list.component';
+import { ProfileComponent } from './profile/profile.component';
+import { FooterComponent } from './footer/footer.component';
+import { PetFormComponent } from './pet-form/pet-form.component';
+import { PetEditComponent } from './pet-edit/pet-edit.component';
+import { PetDetailsComponent } from './pet-details/pet-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainPageComponent,
+    NavBarComponent,
+    PetListComponent,
+    ProfileComponent,
+    FooterComponent,
+    PetFormComponent,
+    PetEditComponent,
+    PetDetailsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
